64c8bbe067f6d590e30515079b24d1691adf4380
Mpcsj <marcosjunioribm@gmail.com>
2024-08-30 19:07:22 -0300

    refactor: Update hello message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World - v2 !';
+    return 'Hello World - v3 !';
   }
 }
