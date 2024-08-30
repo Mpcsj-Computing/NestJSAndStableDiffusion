7a221036fd61eed06aca70ea751bd9189cda8e64
Mpcsj <marcosjunioribm@gmail.com>
2024-08-30 18:24:58 -0300

    feat: New route for Stability Ultra model :sparkles:

--- src/stable-difusion-integration/stable-difusion-integration.controller.ts
+++ src/stable-difusion-integration/stable-difusion-integration.controller.ts
@@ -1,18 +1,32 @@
 import { Body, Controller, Post, Res, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { createReadStream } from 'fs';
-import { GenerateImageDto } from './generate-image.dto';
+import {
+  GenerateImageDto,
+  GenerateImageFromPromptDto,
+} from './generate-image.dto';
 import { StableDifusionIntegrationService } from './stable-difusion-integration.service';
 
 @Controller('stable-difusion-integration')
 export class StableDifusionIntegrationController {
-    constructor(private readonly service:StableDifusionIntegrationService){}
+  constructor(private readonly service: StableDifusionIntegrationService) {}
 
-    @Post()
-    @UseInterceptors(FileInterceptor('file'))
-    async generateImage(@Body() data:GenerateImageDto,@Res() res: any){
-        const filePath = await this.service.generateImage(data)
-        const file = createReadStream(filePath)
-        file.pipe(res)
-    }
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async generateImage(@Body() data: GenerateImageDto, @Res() res: any) {
+    const filePath = await this.service.generateImage(data);
+    const file = createReadStream(filePath);
+    file.pipe(res);
+  }
+
+  @Post('/ultra')
+  @UseInterceptors(FileInterceptor('file'))
+  async generateImageWUltraModel(
+    @Body() data: GenerateImageFromPromptDto,
+    @Res() res: any,
+  ) {
+    const filePath = await this.service.generateImageWithUltraModel(data);
+    const file = createReadStream(filePath);
+    file.pipe(res);
+  }
 }
--- src/stable-difusion-integration/stable-difusion-integration.service.ts
+++ src/stable-difusion-integration/stable-difusion-integration.service.ts
@@ -1,58 +1,100 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { lastValueFrom } from 'rxjs';
-import { GenerateImageDto } from './generate-image.dto';
-import {getDataFolderPath,persistData} from "../utils/file.utils"
+import { firstValueFrom, lastValueFrom } from 'rxjs';
+import {
+  GenerateImageDto,
+  GenerateImageFromPromptDto,
+} from './generate-image.dto';
+import { getDataFolderPath, persistData } from '../utils/file.utils';
+import FormData from 'form-data';
 
+const STABLE_DIFUSION_ULTRA_MODEL_URL =
+  'https://api.stability.ai/v2beta/stable-image/generate/ultra';
 
 @Injectable()
 export class StableDifusionIntegrationService {
-    constructor(private readonly httpService:HttpService){}
+  constructor(private readonly httpService: HttpService) {}
 
+  async generateImageWithUltraModel(input: GenerateImageFromPromptDto) {
+    console.log('generateImageWithUltraModel>>', {
+      prompt: input.prompt,
+    });
 
+    const payload = {
+      prompt: input.prompt,
+      output_format: 'png',
+    };
 
-    async generateImage(generateImage:GenerateImageDto){
-        const engineId = 'stable-diffusion-512-v2-0';
+    // Convert payload to FormData
+    const formData = new FormData();
+    Object.entries(payload).forEach(([key, value]) => {
+      formData.append(key, value as string);
+    });
 
-        const prompts:any[] = [
-            {
-                text:generateImage.prompt,
-                weight:1
-            }
-        ]
+    // Make the POST request
+    const data = await firstValueFrom(
+      this.httpService.post(STABLE_DIFUSION_ULTRA_MODEL_URL, formData, {
+        validateStatus: () => true, // Equivalent to `validateStatus: undefined`
+        responseType: 'arraybuffer',
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+          Accept: 'image/*',
+          ...formData.getHeaders(), // Set the correct form-data headers
+        },
+        data: formData.getBuffer(), // Ensure the data is a Buffer
+      }),
+    );
 
-        if(generateImage.negativePrompt){
-            prompts.push({
-                text:generateImage.negativePrompt,
-                weight:-1
-            })
-        }
-        const result = await this.httpService.post(
-            `https://api.stability.ai/v1beta/generation/${engineId}/text-to-image`,
-            {
-              cfg_scale: 7,
-              clip_guidance_preset: 'FAST_BLUE',
-              height:   generateImage.imageHeight,
-              width: generateImage.imageWidth,
-              samples: 1,
-              steps: 50,
-              text_prompts:prompts ,
-            },
-            {
-              headers: {
-                'Content-Type': 'application/json',
-                Accept: 'image/png',
-                Authorization: process.env.API_KEY,
-              },
-              responseType: 'arraybuffer',
-            },
-          );
+    // const data = await lastValueFrom(response);
+    const filePath = `${getDataFolderPath()}/${Date.now()}.png`;
+    persistData(Buffer.from(data.data), filePath);
 
-        const data = await lastValueFrom(result);
-        const filePath = `${getDataFolderPath()}/${Date.now()}.png`
-        persistData(Buffer.from(data.data),filePath)
+    console.log(filePath);
+    return filePath;
+  }
 
-        console.log(filePath)
-        return filePath
+  async generateImage(generateImage: GenerateImageDto) {
+    const engineId = 'stable-diffusion-512-v2-0';
+
+    const prompts: any[] = [
+      {
+        text: generateImage.prompt,
+        weight: 1,
+      },
+    ];
+
+    if (generateImage.negativePrompt) {
+      prompts.push({
+        text: generateImage.negativePrompt,
+        weight: -1,
+      });
     }
+    const result = await this.httpService.post(
+      `https://api.stability.ai/v1beta/generation/${engineId}/text-to-image`,
+      {
+        cfg_scale: 7,
+        clip_guidance_preset: 'FAST_BLUE',
+        height: generateImage.imageHeight,
+        width: generateImage.imageWidth,
+        samples: 1,
+        steps: 50,
+        text_prompts: prompts,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'image/png',
+          Authorization: process.env.API_KEY,
+        },
+        responseType: 'arraybuffer',
+      },
+    );
+
+    const data = await lastValueFrom(result);
+    const filePath = `${getDataFolderPath()}/${Date.now()}.png`;
+    persistData(Buffer.from(data.data), filePath);
+
+    console.log(filePath);
+    return filePath;
+  }
 }
